72edd3498406af6cc09ff6c9ec75c2852ab456aa
PaulAyling <jimmyukka@gmail.com>
2021-03-16 14:34:55 +0100

    Part4 completed

--- main.js
+++ main.js
@@ -9,21 +9,21 @@ const myWalletAddress = myKey.getPublic('hex');
 let paulCoin = new Blockchain();
 const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
 tx1.signTransaction(myKey);
+paulCoin.addTransaction(tx1)
 
 console.log('/n Starting the miner.......');
+paulCoin.minePendingTransactions(myWalletAddress);
 
-paulCoin.minePendingTransactions('Nigels address');
-
-console.log('\n Balnace of Nigels is', paulCoin.getBalanceOfAddress(myWalletAddress));
+console.log('\n Balance of myWallet', paulCoin.getBalanceOfAddress(myWalletAddress));
 
 console.log('/n Starting the miner again ......');
 paulCoin.minePendingTransactions(myWalletAddress);
 
-// console.log('\n Balance of Nigels:', paulCoin.getBalanceOfAddress('Nigels address'));
-// console.log('\n Balance of add_1:', paulCoin.getBalanceOfAddress('add_1'));
-// console.log('\n Balance of add_2:', paulCoin.getBalanceOfAddress('add_2'));
+
 // console.log('----------------------------------------------');
-// console.log('Entire Blockchian', paulCoin.chain);
-// console.log('genisis trans', paulCoin.chain[0].transactions);
-// console.log('Trans 1', paulCoin.chain[1].transactions);
-// console.log('Trans 2', paulCoin.chain[2].transactions);
+console.log('Entire Blockchian', paulCoin.chain);
+console.log('genisis trans', paulCoin.chain[0].transactions);
+console.log('Trans 1', paulCoin.chain[1].transactions);
+console.log('Trans 2', paulCoin.chain[2].transactions);
+
+console.log('\n FINAL :Balance of myWallet', paulCoin.getBalanceOfAddress(myWalletAddress));
